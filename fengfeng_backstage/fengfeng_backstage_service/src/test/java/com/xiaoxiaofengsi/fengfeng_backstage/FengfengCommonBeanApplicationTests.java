305bafcd31290d34e93f827e82ed4516ab731d90
wangsh8 <wangsh8@assiainfo.com>
2018-04-30 00:27:01 +0800

    集成mybatis+dubbo+redis

--- fengfeng_backstage/fengfeng_backstage_service/src/test/java/com/xiaoxiaofengsi/fengfeng_backstage/FengfengCommonBeanApplicationTests.java
+++ fengfeng_backstage/fengfeng_backstage_service/src/test/java/com/xiaoxiaofengsi/fengfeng_backstage/FengfengCommonBeanApplicationTests.java
@@ -1,10 +1,13 @@
 package com.xiaoxiaofengsi.fengfeng_backstage;
 
+import com.xiaoxiaofengsi.fengfeng_backstage.redisDao.RedisDao;
 import com.xiaoxiaofengsi.fengfeng_backstage.service.UserFirstService;
 import com.xiaoxiaofengsi.fengfeng_backstage.waientity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
@@ -19,9 +22,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes=FengfengBackstageSerApplication.class)
 public class FengfengCommonBeanApplicationTests {
 
+	public static Logger logger= LoggerFactory.getLogger(FengfengCommonBeanApplicationTests.class);
 	@Autowired
 	private UserFirstService service;
 
+
+	@Autowired
+	RedisDao redisDao;
+	@Test
+	public void testRedis(){
+		redisDao.setKey("name","forezp");
+		redisDao.setKey("age","11");
+		logger.info(redisDao.getValue("name"));
+		logger.info(redisDao.getValue("age"));
+	}
 
 	@Test
 	public void contextLoads() {
@@ -32,7 +46,7 @@ public class FengfengCommonBeanApplicationTests {
 	public void insertUserFirst() {
 
 		User user = new User();
-		user.setUserId(123);
+		user.setUserId(66123);
 		user.setUsername("wsh");
 		user.setPassword("123456");
 		user.setRemark("第一次该springboot使用数据库");
